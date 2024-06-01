17d534ff85631161cfd3f4cee7566c348431a0d5
VivaanRK <106665086+Hecker0ff1c1al@users.noreply.github.com>
2024-06-01 13:12:41 +0100

    Add files via upload

---
+++ News-Flasher/script.js
@@ -0,0 +1,8 @@
+// script.js
+window.addEventListener('blur', function() {
+    document.title = 'Get back to reading!';
+});
+
+window.addEventListener('focus', function() {
+    document.title = 'The News Flasher!';
+});
